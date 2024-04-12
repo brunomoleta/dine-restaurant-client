bee0a89f07dd867efa0a82e04e8dcd0a9ebec92a
Bruno Moleta <brunomoleta@pm.me>
2024-04-12 18:57:36 -0300

    Add form component
    This commit implements a new form component, complete with styles and tests. The form fields include name, email, time, date, and number of people.

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,52 @@
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+
+  selector: 'reservation-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+
+export class FormComponent {
+
+  defaultValues: DefaultValuesI = {
+    name: "",
+    email: "",
+    time: "19:00",
+    numberPeople: 2,
+    date: new Date()
+  }
+  formValues: DefaultValuesI = {...this.defaultValues}
+
+
+  onNameChange(newName: string) {
+    this.formValues.name = newName
+  }
+
+  onEmailChange(newEmail: string) {
+    this.formValues.email = newEmail
+  }
+
+  onTimeChange(newTime: string) {
+    this.formValues.time = newTime
+  }
+
+  onDateChange(newTime: Date) {
+    this.formValues.date = newTime
+  }
+
+  onSubmit() {
+    this.formValues = {...this.defaultValues}
+  }
+}
+
+interface DefaultValuesI {
+  name: string;
+  email: string;
+  time: string;
+  date: Date;
+  numberPeople: number;
+}
